388a1d538535b5c85d18bc283bbbe4ac4d17e452
MOEEZ <thetruthes1234@gmail.com>
2026-07-23 20:21:59 +0500

    Add files via upload
    programing journey

---
+++ function_testing.py
@@ -0,0 +1,8 @@
+def print_hi(name=""):
+    greet=f'Hello {name}!'
+    return greet
+to=print_hi("moeez")
+print(to)
+def mathformula(a,b,c):
+    return a+b+c
+
---
+++ importing_self_function.py
@@ -0,0 +1,6 @@
+from function_testing import print_hi
+greetings=print_hi('MOEEZ')
+print(greetings)
+from function_testing import mathformula
+formula=mathformula(4,4,4)
+print(formula)
