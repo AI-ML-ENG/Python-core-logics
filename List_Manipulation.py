7ab9b41471929518be499e61013a542b90baea6f
MOEEZ <thetruthes1234@gmail.com>
2026-08-13 23:01:28 +0500

    Add files via upload

---
+++ List_Manipulation.py
@@ -0,0 +1,11 @@
+lists=[10,20,30,40,50,60,70,80]
+tuples=('math','science','english','history','geography')
+strings='Python programing'
+print(lists[2],tuples[2],strings[2])
+slicing_list=lists[0:8]
+print(slicing_list)
+slicing_tuple=tuples[2:5]
+print(slicing_tuple)
+listss=lists[-8:-1]
+print(listss)
+print(strings[-2:])
